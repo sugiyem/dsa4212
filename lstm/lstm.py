b2f3517a1fc2b3c69225220c0163c1cb4be68d03
Wilson W <wilsonwidyadhana681@gmail.com>
2024-03-03 18:31:48 +0800

    Add LSTM and lstm_test

--- lstm/lstm.py
+++ lstm/lstm.py
@@ -3,16 +3,22 @@ import jax.numpy as jnp
 from jax.typing import ArrayLike
 from jax import Array
 
+RANDOM_SEED = 42
+
 @jax.jit
 def sigmoid(x: ArrayLike) -> ArrayLike:
     return 1./(1 + jnp.exp(-x))
 
-@jax.jit
 def rng_unif(key: Array, shape: tuple[int, int]) -> ArrayLike:
     return jax.random.uniform(key=key, shape=shape)
 
 class LSTM:
-    def __init__(self, seed: int, input_dim: int, hidden_dim: int):
+    def __init__(
+        self, 
+        seed: int, 
+        input_dim: int, 
+        hidden_dim: int
+    ):
         self.key = jax.random.PRNGKey(seed)
         self.wf = rng_unif(key=self.key, shape=(hidden_dim, hidden_dim + input_dim))
         self.bf = rng_unif(key=self.key, shape=(hidden_dim,))
@@ -51,7 +57,12 @@ class LSTM:
         return (self.c_cur(x_cur, h_prev, c_prev), self.h_cur(x_cur, h_prev, c_prev))
 
 class PeepholeLSTM(LSTM):
-    def __init__(self, seed: int, input_dim: int, hidden_dim: int):
+    def __init__(
+        self, 
+        seed: int, 
+        input_dim: int, 
+        hidden_dim: int
+    ):
         super().__init__(seed, input_dim, hidden_dim)
         self.wf = rng_unif(key=self.key, shape=(hidden_dim, 2 * hidden_dim + input_dim))
         self.wi = rng_unif(key=self.key, shape=(hidden_dim, 2 * hidden_dim + input_dim))
@@ -77,3 +88,41 @@ class PeepholeLSTM(LSTM):
     def forward(self, x_cur: ArrayLike, h_prev: ArrayLike, c_prev: ArrayLike) -> tuple[ArrayLike, ArrayLike]:
         c_t = self.c_cur(x_cur, h_prev, c_prev)
         return (c_t, self.h_cur(x_cur, h_prev, c_prev, c_t))
+
+
+class LSTMModel:
+    def __init__(
+        self,
+        num_layers: int,
+        lstm_type: str,
+        input_dim: int,
+        output_dim: int,
+        hidden_dim: int,
+        seed: int = RANDOM_SEED
+    ):
+        self.num_layers = num_layers
+        self.layers = []
+        self.seed = seed
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.hidden_dim = hidden_dim
+        if (lstm_type == "vanilla"):
+            self.layers = [LSTM(
+                seed=self.seed, 
+                input_dim=self.input_dim, 
+                hidden_dim=self.hidden_dim) 
+                for _ in range(self.num_layers)]
+        elif (lstm_type == "peephole"):
+            self.layers = [PeepholeLSTM(
+                seed=self.seed,
+                input_dim=self.input_dim, 
+                hidden_dim=self.hidden_dim)
+                for _ in range(self.num_layers)]
+        else:
+            raise ValueError("lstm_type must be 'vanilla' or 'peephole'.")
+        self.key = jax.random.PRNGKey(seed=self.seed)
+        self.w_out = rng_unif(key=self.key, shape=(self.hidden_dim, self.output_dim))
+        self.b_out = rng_unif(key=self.key, shape=(self.hidden_dim,))
+    
+    def forward(self, x: ArrayLike) -> ArrayLike:
+        pass
