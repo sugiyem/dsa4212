ede532c71eb14234128bc0c9db571e0b5837e913
Wilson W <wilsonwidyadhana681@gmail.com>
2024-03-10 22:30:38 +0800

    Add transformer code

--- transformers/transformers.py
+++ transformers/transformers.py
@@ -1,3 +1,31 @@
 import jax
 import jax.numpy as jnp
+from utils import softmax
 
+class Attention:
+    @staticmethod
+    @jax.jit
+    def calc_attention(
+        query: jnp.ndarray,
+        key: jnp.ndarray,
+        value: jnp.ndarray
+    ) -> jnp.ndarray:
+        dk = query.shape[0]
+        return softmax((query @ key.T)/jnp.sqrt(dk)) @ value
+
+    @staticmethod
+    @jax.jit
+    def calc_masked_attention(
+        query: jnp.ndarray,
+        key: jnp.ndarray,
+        value: jnp.ndarray
+    ) -> jnp.ndarray:
+        pass
+
+    @staticmethod
+    @jax.jit
+    def calc_multi_head_attention(
+        query: jnp.ndarray,
+        key: jnp.ndarray,
+        value: jnp.ndarray
+    )
---
+++ transformers/utils.py
@@ -0,0 +1,29 @@
+import jax
+import jax.numpy as jnp
+from typing import NamedTuple
+
+class InputParams(NamedTuple):
+    wq: jnp.ndarray
+    wk: jnp.ndarray
+    wv: jnp.ndarray
+
+class SingleHeadAttnParams(NamedTuple):
+    wq_i: jnp.ndarray
+    wk_i: jnp.ndarray
+    wv_i: jnp.ndarray
+
+class MultiHeadAttnParams(NamedTuple):
+    w_full: list[SingleHeadAttnParams]
+    wo: jnp.ndarray
+
+@jax.jit
+def softmax(x: jnp.ndarray) -> jnp.ndarray:
+    x_exp = jnp.exp(x)
+    return x_exp / jnp.sum(x_exp)
+
+@jax.jit
+def calc_inputs(params: InputParams, x: jnp.ndarray) -> jnp.ndarray:
+    q = x @ params.wq
+    k = x @ params.wk
+    v = x @ params.wv
+    return jnp.array([q, k, v])
