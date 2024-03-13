c0a5d47de914d0c14b1ec420d189cf8322692618
Kevin Christian <kevinchs0808@Kevins-MacBook-Pro.local>
2024-03-13 23:24:03 +0800

    Added some bipartite graph modelling classes for collaborative filtering

---
+++ colab-filter/lightgcn.py
@@ -0,0 +1,26 @@
+# Credit: 
+
+# Code Link: https://colab.research.google.com/drive/1VQTBxJuty7aLMepjEYE-d7E9kjo51CA1?usp=sharing#scrollTo=Y9fonQcxt3do
+# Authors: Derrick Li, Peter Maldonado, Akram Sbaih [Stanford, CS224W]
+
+import torch_geometric
+from torch_geometric.nn.conv import MessagePassing
+from torch_geometric.utils import degree
+
+class LightGCNConv(MessagePassing):
+  def __init__(self, **kwargs):  
+    super().__init__(aggr='add')
+
+  def forward(self, x, edge_index):
+    # Compute normalization
+    from_, to_ = edge_index
+    deg = degree(to_, x.size(0), dtype=x.dtype)
+    deg_inv_sqrt = deg.pow(-0.5)
+    deg_inv_sqrt[deg_inv_sqrt == float('inf')] = 0
+    norm = deg_inv_sqrt[from_] * deg_inv_sqrt[to_]
+
+    # Start propagating messages (no update after aggregation)
+    return self.propagate(edge_index, x=x, norm=norm)
+
+  def message(self, x_j, norm):
+    return norm.view(-1, 1) * x_j
