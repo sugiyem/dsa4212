4f87fa3200a23ee231e7f1d18a2e9fa2f10679f9
Farrel Dwireswara Salim <90848662+sugiyem@users.noreply.github.com>
2024-03-22 15:37:07 +0800

    Complete Attention class implementation

--- transformers/transformers.py
+++ transformers/transformers.py
@@ -7,7 +7,7 @@ class Attention:
     @jax.jit
     def init_input_params(
         key: jax.Array,
-        input_dim: int, 
+        input_dim: int,
         dk: int, 
         dv: int
     ) -> InputParams:
@@ -19,10 +19,9 @@ class Attention:
     @staticmethod
     @jax.jit
     def calc_attention(
-        params: InputParams,
         query: jnp.ndarray,
         key: jnp.ndarray,
-        value: jnp.ndarray
+        value: jnp.ndarray,
     ) -> jnp.ndarray:
         dk = query.shape[0]
         return softmax((query @ key.T)/jnp.sqrt(dk)) @ value
@@ -30,19 +29,36 @@ class Attention:
     @staticmethod
     @jax.jit
     def calc_masked_attention(
-        params: InputParams,
         query: jnp.ndarray,
         key: jnp.ndarray,
-        value: jnp.ndarray
+        value: jnp.ndarray,
+        mask: jnp.ndarray
     ) -> jnp.ndarray:
-        pass
-
+        dk = query.shape[0]
+        scores = (query @ key.T) / jnp.sqrt(dk)
+        masked_scores = jnp.where(mask == 0, -jnp.inf, scores)
+        return softmax(masked_scores) @ value
+    
     @staticmethod
     @jax.jit
     def calc_multi_head_attention(
         params: MultiHeadAttnParams,
         query: jnp.ndarray,
         key: jnp.ndarray,
-        value: jnp.ndarray
+        value: jnp.ndarray,
+        mask: jnp.ndarray = None
     ) -> jnp.ndarray:
-        pass
+        concat_attention = jnp.array([])
+
+        for attention_param in params.w_full:
+            wq_i, wk_i, wv_i = attention_param
+            q_i = query @ wq_i 
+            k_i = key @ wk_i 
+            v_i = value @ wv_i
+
+            scaled_attention = Attention.calc_attention(q_i, k_i, v_i) if mask is None \
+                else Attention.calc_masked_attention(q_i, k_i, v_i, mask)
+            concat_attention = jnp.append(concat_attention, scaled_attention)
+        
+        return concat_attention @ params.wo
+
